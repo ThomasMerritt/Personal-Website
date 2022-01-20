31254c516e45f45b419db5fab141432dad5cc578
ThomasMerritt <84432380+ThomasMerritt@users.noreply.github.com>
2022-01-19 22:59:09 -0800

    More files
    Added both css and js files

---
+++ asset.js
@@ -0,0 +1,14 @@
+const mine = document.querySelector(".mine");  
+const slider = document.querySelector(".slider");  
+const logo = document.querySelector(".logo");  
+const box = document.querySelector(".box");  
+const dream = document.querySelector(".dream"); 
+
+const tl = new TimelineMax();
+
+
+tl.fromTo(mine,1, {height: "15%"}, {height: "80%", ease: Power2.easeInOut})
+.fromTo(slider,1, {x:"-100%"}, {x: "0%", ease: Power2.easeInOut}, "-=.8");
+
+let hamburger = document.getElementById('hamburger');
+hamburger.classList.toggle('fade');
