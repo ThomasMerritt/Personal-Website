ea898526b265d9b8edb259aa8a03cfd9bf356993
ThomasMerritt <84432380+ThomasMerritt@users.noreply.github.com>
2022-01-25 00:16:03 -0800

    Changed mine to canvas, also implemented overflow hidden

--- asset.js
+++ asset.js
@@ -1,10 +1,10 @@
-const mine = document.querySelector(".mine");  
+const canvas = document.querySelector(".canvas");  
 const slider = document.querySelector(".slider");  
 
 const tl = new TimelineMax();
 
 
-tl.fromTo(mine,1.5, {height: "15%"}, {height: "80%", ease: Power2.easeInOut})
+tl.fromTo(canvas,1.5, {height: "15%"}, {height: "80%", ease: Power2.easeInOut})
 .fromTo(slider,1, {x:"-100%"}, {x: "0%", ease: Power2.easeInOut}, "-=.8");
 
 let hamburger = document.getElementById('hamburger');
