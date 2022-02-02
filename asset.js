7b47f8930d9a4b4537e169d83ceec382c8e844a9
ThomasMerritt <84432380+ThomasMerritt@users.noreply.github.com>
2022-02-01 22:05:32 -0800

    Animation, Title, Link, and screen optimize
    Optimized screen for 1920px pc, also edited animation timing, added more links to nav, and added a title for the website tab to say "Thomas Merritt"

--- asset.js
+++ asset.js
@@ -1,11 +1,13 @@
 const canvas = document.querySelector(".canvas");  
 const slider = document.querySelector(".slider");  
+const navlinks = document.querySelector(".nav-links");
+let hamburger = document.getElementById('hamburger');
+let link = document.getElementById('link');
 
 const tl = new TimelineMax();
 
-
 tl.fromTo(canvas,1.5, {height: "15%"}, {height: "80%", ease: Power2.easeInOut})
+.fromTo(navlinks,.75, {height: "0%"}, {height: "8%"})
 .fromTo(slider,1, {x:"-100%"}, {x: "0%", ease: Power2.easeInOut}, "-=.8");
 
-let hamburger = document.getElementById('hamburger');
-hamburger.classList.toggle('fade'); 
+
