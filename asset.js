f9e8dbdd35a60da29998f6c0294b382bb7cfe7ad
ThomasMerritt <84432380+ThomasMerritt@users.noreply.github.com>
2022-02-06 13:53:29 -0800

    Index, asset, and style change
    added scrollEffect for assest.js, changed YOUTUBE to MUSIC in HTML,  changed screen cap from min to max in css

--- asset.js
+++ asset.js
@@ -2,7 +2,6 @@ const canvas = document.querySelector(".canvas");
 const navlinks = document.querySelector(".nav-links");
 let dream = document.getElementById('dream');
 let slider = document.getElementById('slider');
-
 const tl = new TimelineMax();
 
 tl.fromTo(canvas,2, {height: "2%"}, {height: "80%", ease: Power3.easeInOut})
@@ -10,3 +9,21 @@ tl.fromTo(canvas,2, {height: "2%"}, {height: "80%", ease: Power3.easeInOut})
 
 slider.classList.toggle('fade');
 dream.classList.toggle('fade');
+
+window.onload = function() {
+    const CONTAINER = document.querySelector('.container')
+
+    window.addEventListener('scroll', scrollEffect);
+
+    function scrollEffect() {
+        if (window.scrollY >= 150) {
+            CONTAINER.style.opacity = '1';
+            CONTAINER.style.transform = 'translateX(0px)';
+            CONTAINER.style.transition = '1s ease-in-out';
+        } else {
+            CONTAINER.style.opacity = '0';
+            CONTAINER.style.transform = 'translate(-20px)';
+        }
+    }
+    scrollEffect();
+}
