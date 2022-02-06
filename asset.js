eb6ca2a468b1064765cab1f4f07b0c8e4e545d5e
ThomasMerritt <84432380+ThomasMerritt@users.noreply.github.com>
2022-02-06 00:57:06 -0800

    More boxes, fade-in links, changed background to fade-in

--- asset.js
+++ asset.js
@@ -1,12 +1,12 @@
 const canvas = document.querySelector(".canvas");  
-const slider = document.querySelector(".slider");  
 const navlinks = document.querySelector(".nav-links");
 let dream = document.getElementById('dream');
+let slider = document.getElementById('slider');
 
 const tl = new TimelineMax();
 
-tl.fromTo(canvas,2, {height: "10%"}, {height: "80%", ease: Power2.easeInOut})
-.fromTo(navlinks,.75, {height: "0%"}, {height: "8%"})
-.fromTo(slider,1.25, {x:"-100%"}, {x: "0%", ease: Power2.easeInOut}, "-=.8");
+tl.fromTo(canvas,2, {height: "2%"}, {height: "80%", ease: Power3.easeInOut})
+.fromTo(navlinks,.75, {height: "0%"}, {height: "8%", ease: Power1.easeInOut})
 
+slider.classList.toggle('fade');
 dream.classList.toggle('fade');
