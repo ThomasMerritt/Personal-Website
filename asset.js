6340b459bcbfa471b64bd337df7f917654adcad7
ThomasMerritt <84432380+ThomasMerritt@users.noreply.github.com>
2022-02-17 20:01:50 -0800

    Change dream to caption, and some css changes
    nav min-height, link z-index, changed caption timing, changed background when zooming in, and some more screen optimization for 769px

--- asset.js
+++ asset.js
@@ -1,14 +1,14 @@
 const canvas = document.querySelector(".canvas");  
 const navlinks = document.querySelector(".nav-links");
-let dream = document.getElementById('dream');
+let caption = document.getElementById('caption');
 let slider = document.getElementById('slider');
 const tl = new TimelineMax();
 
-tl.fromTo(canvas,2, {height: "2%"}, {height: "80%", ease: Power3.easeInOut})
+tl.fromTo(canvas,2, {height: "0%"}, {height: "80%", ease: Power3.easeInOut})
 .fromTo(navlinks,.75, {height: "0%"}, {height: "8%", ease: Power1.easeInOut})
 
 slider.classList.toggle('fade');
-dream.classList.toggle('fade');
+caption.classList.toggle('fade');
 
 window.onload = function() {
     const CONTAINER = document.querySelector('.container')
